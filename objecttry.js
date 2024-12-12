8cac771978317d19e5221dbecb9fb5a5afaef15a
MD MEHEDI HASAN <55634986+Mehedi16009@users.noreply.github.com>
2024-12-12 13:34:16 +0600

    Add files via upload

---
+++ objecttry.js
@@ -0,0 +1,17 @@
+let lastName = 'Mehedi';
+let oldLastName = lastName;
+
+lastName = 'HAsan';
+
+console.log(lastName, oldLastName);
+
+const anika = {
+  firstName: 'Anika',
+  lastName: 'Sadia',
+  age: 21,
+};
+
+const marriedAnika = anika;
+marriedAnika.lastName = 'Hasan';
+console.log('Before marriage :', jessica);
+console.log('After marriage:', marriedAnika);
